6f567b79bf910be71a507059d2b40a055809d4a1
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-07-02 00:06:37 +0300

    wrote the get_all_vacancies function

--- src/api_hh.py
+++ src/api_hh.py
@@ -27,3 +27,27 @@ class HHApi:
         response = requests.get("https://api.hh.ru/vacancies", params=params)
         if response.status_code == 200:
             return response.json()["items"]
+
+
+
+    def get_all_vacancies(self):
+        """сохранение списка вакансий"""
+        employers = self.get_employers()
+        all_vacancies = []
+        for employer in employers:
+            vacancies = self.__get_vacancies_on_employer(employer["id"])
+            for vacancy in vacancies:
+                if vacancy["salary"] is None:
+                    salary_from = 0
+                    salary_to = 0
+                else:
+                    salary_from = vacancy["salary"]["from"] if vacancy["salary"]["from"] else 0
+                    salary_to = vacancy["salary"]["to"] if vacancy["salary"]["to"] else 0
+                all_vacancies.append({"id": vacancy["id"],
+                                      "name": vacancy["name"],
+                                      "url": vacancy["alternate_url"],
+                                      "salary_from": salary_from,
+                                      "salary_to": salary_to,
+                                      "employer": employer["id"],
+                                      "area": vacancy["area"]["name"]})
+        return all_vacancies
